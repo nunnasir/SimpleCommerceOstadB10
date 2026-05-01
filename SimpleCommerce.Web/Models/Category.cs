7adf653a0b218686cbfa822e62ab001e48435ddb
nasir uddin <nunnasir@gmail.com>
2026-05-01 23:02:32 +0600

    EF Configuration

---
+++ SimpleCommerce.Web/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿namespace SimpleCommerce.Web.Models;
+
+public class Category
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public int CreatedBy { get; set; }
+    public DateTime UpdatedAt { get; set; }
+    public int UpdatedBy { get; set; }
+}
