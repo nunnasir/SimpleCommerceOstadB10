fefe0cd8e0e99ad48a87e6e5faa54b1ef7f3f974
nasir uddin <nunnasir@gmail.com>
2026-05-01 21:49:04 +0600

    Add project files.

---
+++ SimpleCommerce.Web/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleCommerce.Web.Areas.Admin.Controllers;
+
+[Area("Admin")]
+public class CategoryController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
